19146edda2c859068279bff3499010adb9073b1c
Alfarisg 34 <alfarisg34@gmail.com>
2023-09-20 19:34:21 +0700

    Learn selenium

---
+++ Belajar Selenium/main.js
@@ -0,0 +1,29 @@
+const {Builder , Browser, By} = require('selenium-webdriver')
+
+
+async function main(){
+    const driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .build()
+
+    await driver.get('https://www.saucedemo.com/v1/')
+    //Login Form
+    await driver.findElement(By.id('user-name')).sendKeys('standard_user')
+    await driver.findElement(By.id('password')).sendKeys('secret_sauce')
+    await driver.findElement(By.id('login-button')).click()
+    //Item to Cart
+    await driver.findElement(By.css('.btn_primary')).click()
+    await driver.findElement(By.css('.btn_primary')).click()
+    await driver.findElement(By.id('shopping_cart_container')).click()
+    await driver.findElement(By.css('.checkout_button')).click()
+    //Information form
+    await driver.findElement(By.id('first-name')).sendKeys('user')
+    await driver.findElement(By.id('last-name')).sendKeys('user')
+    await driver.findElement(By.id('postal-code')).sendKeys('12345')
+    await driver.findElement(By.css('.cart_button')).click()
+    await driver.findElement(By.css('.cart_button')).click()
+    //Logout
+    await driver.findElement(By.css('.bm-burger-button')).click() 
+    await driver.findElement(By.id('logout_sidebar_link')).click()
+}
+main()
