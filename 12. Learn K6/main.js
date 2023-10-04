28465d30d7a2e222cabb8a865271f52fbe551723
Alfarisg 34 <alfarisg34@gmail.com>
2023-10-04 13:31:47 +0700

    main

--- 12. Learn K6/main.js
+++ 12. Learn K6/main.js
@@ -1,25 +1,94 @@
 import http from 'k6/http'
-import { check } from 'k6'
+import { check, group } from 'k6'
+import { Rate, Trend } from 'k6/metrics'
+import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
+
+const completedRate = new Rate('todo_completed_rate')
+const todosAllDuration = new Trend('todos_all_duration')
+const todosDetailDuration = new Trend('todos_detail_duration')
 
 export const options = {
-    thresholds: {
-		http_req_duration: ['avg<300', 'p(90)<250'],
-		http_req_failed: ['rate<0.5'],
-		iterations: ['count>400']
+	thresholds: {
+		http_req_duration: ['avg<3000', 'p(90)<2500'],
+		http_req_failed: ['rate<0.01'],
+		iterations: ['count>40']
 	},
-    stages: [
-		{ duration: '10s', target: 10 },
-		{ duration: '5s', target: 10 },
-		{ duration: '2s', target: 15 },
-		{ duration: '5s', target: 0 },
-	]
+	stages: [
+		{ duration: '2s', target: 5 },
+		{ duration: '2s', target: 10 },
+		{ duration: '2s', target: 10 },
+		{ duration: '2s', target: 0 },
+	],
+	// duration: '10s',
+	// iterations: 1,
+	// vus: 1,
 }
 
 export default function () {
-	const res = http.get('https://dummyjson.com/products?limit=10&skip=10&select=title,price')
-
-    check(res, {
-		'responsenya harus 200': (r) => r.status === 200,
-		'di dalamnya harus ada products': (r) => 'products' in r.json()
+	describe('Performance Testing Website Youtube', function () {
+		group('Home Page', function () {
+			const res = http.get('https://www.youtube.com/')
+			check(res, {
+				'Status = 200': (r) => r.status === 200,
+				'Status Text = "200 OK"': (r) => r.status_text === "200 OK",
+				'Request URL = "https://www.youtube.com/"': (r) => r.request.url === "https://www.youtube.com/",
+			})
+			expect(res.status, 'Status').to.equal(200)
+			expect(res.status_text, 'Status Text').to.equal("200 OK")
+			expect(res.url, 'URL').to.equal("https://www.youtube.com/")
+		})
+		group('Youtuber Tech GadgetIn', function () {
+			const res = http.get('https://www.youtube.com/@GadgetIn', {
+				tags: {
+					genre: "Tech"
+				}
+			})
+			check(res, {
+				'Status = 200': (r) => r.status === 200,
+				'Status Text = "200 OK"': (r) => r.status_text === "200 OK",
+				'Request URL = "https://www.youtube.com/@GadgetIn"': (r) => r.request.url === "https://www.youtube.com/@GadgetIn",
+			}, {
+				genre: "Tech"
+			})
+			expect(res.status, 'Status').to.equal(200)
+			expect(res.status_text, 'Status Text').to.equal("200 OK")
+			expect(res.url, 'URL').to.equal("https://www.youtube.com/@GadgetIn")
+			// expect(res.genre, 'Genre').to.equal("Tech")
+		})
+		group('Youtuber Gaming Miawaug', function () {
+			const res = http.get('https://www.youtube.com/@Miawaug', {
+				tags: {
+					genre: "Gaming"
+				}
+			})
+			check(res, {
+				'Status = 200': (r) => r.status === 200,
+				'Status Text = "200 OK"': (r) => r.status_text === "200 OK",
+				'Request URL = "https://www.youtube.com/@Miawaug"': (r) => r.request.url === "https://www.youtube.com/@Miawaug",
+			}, {
+				genre: "Gaming"
+			})
+			expect(res.status, 'Status').to.equal(200)
+			expect(res.status_text, 'Status Text').to.equal("200 OK")
+			expect(res.url, 'URL').to.equal("https://www.youtube.com/@Miawaug")
+			// expect(res.genre, 'Genre').to.equal("Gaming")
+		})
 	})
+	describe('Performance Testing Website dummyjson', function () {
+		group('Test Rate dummyjson todos', function () {
+			for (let i = 1; i < 10; i++) {
+				const res = http.get('https://dummyjson.com/todos/' + i)
+				completedRate.add(res.json().completed)
+			}
+		})
+		group('Test Trend dummyjson todos', function () {
+			const resAll = http.get('https://dummyjson.com/todos')
+			const resDetail = http.get('https://dummyjson.com/todos/1')
+
+			todosAllDuration.add(resAll.timings.duration)
+			todosDetailDuration.add(resDetail.timings.duration)
+		})
+	})
+
+
 }
