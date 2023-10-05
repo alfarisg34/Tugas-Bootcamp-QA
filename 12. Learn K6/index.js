830e790ab7834237a106c1010d45bc21f2f721bd
Alfarisg 34 <alfarisg34@gmail.com>
2023-10-05 19:41:35 +0700

    minor change

--- 12. Learn K6/index.js
+++ 12. Learn K6/index.js
@@ -3,11 +3,12 @@ import { group, sleep } from 'k6'
 
 import thresholds from './config/thresholds.js'
 import smoke_test_scenario from './config/smoke_test_scenario.js'
-import average_load_test_scenario from  './config/average_load_test_scenario.js'
+import average_load_test_scenario from './config/average_load_test_scenario.js'
 
 import getAuthToken from './utils/getAuthToken.js'
 
-import withLogin from './groups/withLogin.js'
+import todos from './groups/todos.js'
+import products from './groups/products.js'
 
 const scenarioList = {
     smoke: smoke_test_scenario,
@@ -21,12 +22,12 @@ export const options = {
     }
 }
 
-export function setup () {
+export function setup() {
     return getAuthToken()
 }
 
-export default function ( token ) {
-    withLogin(token)
-
+export default function (token) {
+    todos(token)
+    products(token)
     sleep(1)
 }
